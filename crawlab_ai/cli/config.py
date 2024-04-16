94492fbb73b177822611d011d0be4fd07793ed6f
Marvin Zhang <tikazyq@163.com>
2024-04-16 21:57:49 +0800

    fix: remove unnecessary args

--- crawlab_ai/cli/config.py
+++ crawlab_ai/cli/config.py
@@ -4,9 +4,9 @@ from crawlab_ai.utils.env import set_api_endpoint
 
 def setup_config_parser(subparsers):
     config_parser = subparsers.add_parser("config", help="Configure crawlab")
-    config_parser.add_argument(
-        "-u", "--url", help="Crawlab API URL", default="http://localhost:8000"
-    )
+    # config_parser.add_argument(
+    #     "-u", "--url", help="Crawlab API URL", default="http://localhost:8000"
+    # )
     config_parser.add_argument("-t", "--token", help="Crawlab API token")
     config_parser.set_defaults(func=config)
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crawlab-ai",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages(),
     url="https://github.com/crawlab-team/crawlab-ai-sdk",
     license="MIT",
