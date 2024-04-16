1cca12ecd7742444cc4589803364a31252677f7a
Marvin Zhang <tikazyq@163.com>
2024-04-16 21:47:32 +0800

    feat: updated cli

--- crawlab_ai/cli/codegen.py
+++ crawlab_ai/cli/codegen.py
@@ -3,31 +3,31 @@ from crawlab_ai.code.list import get_code_list
 from crawlab_ai.utils.auth import get_token
 
 
-def setup_gen_code_parser(subparsers):
-    gen_code_parser = subparsers.add_parser(
+def setup_codegen_parser(subparsers):
+    codegen_parser = subparsers.add_parser(
         "gen_code", help="Generate crawler code for a webpage"
     )
-    gen_code_parser.add_argument("url", help="URL to generate code for")
-    gen_code_parser.add_argument(
+    codegen_parser.add_argument("url", help="URL to generate code for")
+    codegen_parser.add_argument(
         "-t",
         "--type",
         help="Type of the webpage to generate code for",
         default="list",
         choices=["article", "list"],
     )
-    gen_code_parser.add_argument("-o", "--output", help="Output file path")
-    gen_code_parser.set_defaults(func=gen_code)
+    codegen_parser.add_argument("-o", "--output", help="Output file path")
+    codegen_parser.set_defaults(func=codegen)
 
 
-def gen_code(args):
+def codegen(args):
     get_token()
     if args.type == "list":
-        gen_code_list(args)
+        codegen_list(args)
     elif args.type == "article":
-        gen_code_article(args)
+        codegen_article(args)
 
 
-def gen_code_list(args):
+def codegen_list(args):
     code = get_code_list(args.url)
     if args.output:
         with open(args.output, "w") as f:
@@ -36,7 +36,7 @@ def gen_code_list(args):
         print(code)
 
 
-def gen_code_article(args):
+def codegen_article(args):
     code = get_code_article(args.url)
     if args.output:
         with open(args.output, "w") as f:
