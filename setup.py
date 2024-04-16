7d8fefd4d47c25b58ce9a5ea967646f7ce2f4028
Marvin Zhang <tikazyq@163.com>
2024-04-16 22:59:53 +0800

    fix: missing required packages

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    install_requires = f.read().split('\n')
+
 setup(
     name="crawlab-ai",
-    version="0.0.8",
+    version="0.0.9",
     packages=find_packages(),
     url="https://github.com/crawlab-team/crawlab-ai-sdk",
     license="MIT",
     author="Marvin Zhang",
     author_email="tikazyq@163.com",
     description="SDK for Crawlab AI",
+    long_description=long_description,
     package_dir={"crawlab_ai": "crawlab_ai"},
     keywords=["crawlab", "ai"],
+    install_requires=install_requires,
     entry_points={
         "console_scripts": [
             "crawlab-ai=crawlab_ai.cli:main",
